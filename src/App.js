e974cd012a53a9a0e04b4b1a3a0789a9d4271e34
manesh <maneshpunde69@gmail.com>
2021-05-13 22:58:38 +0530

    My first react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent';
+import UpdateEmlpoyeeComponent from './components/UpdateEmlpoyeeComponent';
+import ViewEmployeeComponent from './components/ViewEmployeeComponent';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        
+          <HeaderComponent/>
+            <div className="container">
+              <Switch>
+                <Route path="/" exact component={ListEmployeeComponent}></Route>
+                <Route path="/employees" exact  component={ListEmployeeComponent}></Route>
+                <Route path="/add-employee/:id" exact  component={CreateEmployeeComponent}></Route>
+                <Route path="/view-employee/:id" exact  component={ViewEmployeeComponent}></Route>
+                {/*<Route path="/update-employee/:id"  exact component={UpdateEmlpoyeeComponent}></Route>*/}
+              
+              </Switch>
+            </div>
+          <FooterComponent/>
+         
+      </Router>
     </div>
   );
 }
